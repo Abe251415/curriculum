aaddd76f76098fffc868f5137147dc482e3462d8
oithunder <90395784+Abe251415@users.noreply.github.com>
2021-09-13 16:47:44 +0900

    課題
    まじよくわからんからも少しアホでもわかるように作ってほしいわ

---
+++ JavaStudy/Task1_1.java
@@ -0,0 +1,24 @@
+
+public class Task1_1{
+    public static void main(String[]args) {
+    
+        int hogeInt = 50;
+
+        String hogeString = "テスト";
+
+        //変数hogeDoubleを初期化している
+        double hogeDouble = 0.2;
+
+        hogeInt = 100;
+
+        hogeString = "合格";
+
+        System.out.println(hogeInt);
+
+        //変数hogeStringの表示
+        System.out.println(hogeString);
+
+        System.out.println(hogeDouble);
+        
+    }
+}
